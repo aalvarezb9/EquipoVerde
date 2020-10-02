932874312e38509743c58ae5eac263f3437c709c
mariafpineda <47511303+mariafpineda@users.noreply.github.com>
2020-10-01 23:38:10 -0600

    Updates

--- sp3/Frontend/js/handlerLogged.js
+++
@@ -1,6 +0,0 @@
-let usuario= document.getElementById("userName");
-let cookies=document.cookie;
-cookies= cookies.split(';');
-cookies = cookies[3].split('=');
-let nombre= cookies[1];
-usuario.textContent=`HOLA, ${nombre.toUpperCase()}`;
